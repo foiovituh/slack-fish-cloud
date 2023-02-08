ff22ce795793b814d61fe3931b3a0cecdaa76c88
foiovituh <vitor@improving.com.br>
2023-02-08 17:43:02 -0300

    include labels to EC2 instance message body

--- jq_queries.sh
+++ jq_queries.sh
@@ -3,5 +3,5 @@
 # Shell Style Guide exception:
 #   In this file you are allowed to exceed 80 characters per line
 ###############################################################################
-readonly EC2_INSTANCE_PROPERTIES_JQ_QUERY='map(.[].id + " | " + .[].time + " | " + .[].type) | join("\\n")';
+readonly EC2_INSTANCE_PROPERTIES_JQ_QUERY='map("ID: \(.[].id) | LaunchTime: \(.[].time) | Type: \(.[].type)") | join("\\n")';
 readonly EC2_ASG_PROPERTIES_JQ_QUERY='map("Name: \(.id) | MinSize: \(.min) | MaxSize: \(.max) | DesiredCapacity: \(.now)") | join("\\n")';
